1141166de52722805dc92ca3e7f2f5b104550b57
nort3 <Kosarev1996>
2016-10-15 02:32:23 +0300

    fix

--- server.js
+++ server.js
@@ -196,7 +196,7 @@ app.post('/registration', function(req, res){
 });
 
 app.post('/user/edit', function(req, res){
-    DbData("UPDATE users SET last_name = "+req.body.lastName+", first_name = "+req.body.firstName+" WHERE token = "+req.body.hash+";", function(data){
+    DbData("UPDATE users SET last_name = '"+req.body.lastName+"', first_name = '"+req.body.firstName+"' WHERE token = '"+req.body.hash+"';", function(data){
         if(data.lenght == 0){
             request = true;
             req.send({ok: true});
